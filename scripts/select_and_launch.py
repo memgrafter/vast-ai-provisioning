2fd64532ebca0267b43885f85321ead8dae39fd8
memgrafter <memgrafter@gmail.com>
2026-05-05 16:12:18 -0700

    Fix launcher script imports

--- scripts/select_and_launch.py
+++ scripts/select_and_launch.py
@@ -18,6 +18,8 @@ from typing import Any
 from urllib.error import HTTPError, URLError
 from urllib.request import Request, urlopen
 
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
+
 from vastai import VastAI
 
 from scripts.monitor_instance_readiness import port_url
